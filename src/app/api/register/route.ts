2da96b23854c717d0c6d7c0d8c2d4113f795250b
mohammad <mohammadalirezaie081@gmail.com>
2025-05-11 23:02:42 +0330

    add new shadcn ui components

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -1,10 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
-import { DefaultResponse } from "../../../../types";
 
-export async function POST(request: NextRequest): Promise<DefaultResponse> {
+
+export async function POST(request: NextRequest) {
   const { email, username, password } = await request.json();
-
-  console.log(email, username, password);
-
   return NextResponse.json({ message: "", isSuccess: true, data: "" });
 }
